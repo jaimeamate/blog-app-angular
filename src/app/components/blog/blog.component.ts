e5f0894752715632e532aa80ed2eceb3365e8a94
Jaime Amate R2D <jaime_amate@roadtodata.com>
2024-02-10 20:14:41 +0100

    Inicializacion array Noticias con 2 por defecto

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Noticia } from '../../interfaces/noticia.interface';
 
 @Component({
   selector: 'app-blog',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
-export class BlogComponent {
+export class BlogComponent{
+  noticias: Noticia[] = [
+    {
+      titulo: "Viaje a la playa",
+      imagen: "url_de_la_imagen_1.jpg",
+      descripcion: "Disfruta de unas vacaciones relajantes en la costa. Arena blanca, aguas cristalinas y un clima perfecto te esperan.",
+      fecha: "2024-07-15"    
+    },
+    {
+      titulo: "Curso de programación en línea",
+      imagen: "url_de_la_imagen_2.jpg",
+      descripcion: "Aprende a programar desde cero con nuestro curso en línea. Python, JavaScript, HTML, CSS y mucho más.",
+      fecha: "2024-03-01"
+    }
+  ]
+  noticiaActual!: Noticia
 
+  
 }
---
+++ src/app/interfaces/noticia.interface.ts
@@ -0,0 +1,6 @@
+export interface Noticia {
+    titulo: string
+    imagen: string
+    descripcion: string
+    fecha: string
+}
